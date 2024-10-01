2dd81264ad90bda227cc81e3270f44fee7749cbc
jrajk <aj_rajkumar@yahoo.com>
2024-10-01 03:08:54 +0000

    Updated

--- src/ui_deploy/page/app.py
+++ src/ui_deploy/page/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-from utils.apigw_handler import get_incidents, get_runbook, alert_remediate
+from utils.apigw_handler import get_incidents, get_runbook, incident_remediate
 from utils.init_session import reset_session
+import pandas as pd
 
 def app_page():
     st.set_page_config(layout="wide")
@@ -9,28 +10,35 @@ def app_page():
             reset_session()
             st.rerun()
         
-    st.title("Incidents created")
+    st.title("Incidents")
     st.write("Here the list of active incidents")
+    st.write("Please select an incident to process by clicking the first column of the row")
     incidents = get_incidents("pending")
-    user_table = incidents['Items']
-    st.dataframe(user_table)
-    colms =  st.columns((1, 2, 2,1, 1,1,1,2,2))
-    fields = ['alarmData', 'event_details', 'sk', "event_status","pk", "account_id","Runbook","Remediate"]
-    for col, field_name in zip(colms, fields):
-        # header
-        col.write(field_name)
+    dfall = pd.DataFrame(incidents['Items'])
+    df = dfall.drop('alarmData', axis=1)
+    df = df.drop('account_id', axis=1)
+    selection = st.dataframe(df, on_select="rerun", selection_mode="single-row", hide_index=True)
+    runbook_action = st.button("Runbook")
+    remediate_action = st.button("Remediate")
+    rows = selection['selection']['rows']
+    pk = None
+    description = None
+    if len(rows) != 0:
+        pk = dfall.iloc[rows[0]]['pk']
+        description = dfall.iloc[rows[0]]['alarmData']['description']
+ 
+    if runbook_action:
+        if pk is None:
+            st.write("Please select an incident")
+            return
+        with st.spinner("Processing"):
+            runbook = get_runbook(pk,description)
+            st.write(runbook['runbook'])
 
-    for x, data in enumerate(user_table):
-        col2, col3, col4, col5, col6,col7,col8,col9  = st.columns((1, 2, 2,1, 1,1,1,2))
-        #col1, col2, col3, col4, col5, col6,col7,col8  = st.columns()
-        #col1.write(x)  # index
-        col2.write(data['alarmData'])  
-        col3.write(data['event_details']) 
-        col4.write(data['sk'])   
-        col5.write(data['event_status'])  
-        col6.write(data['pk'])   
-        col7.write(data['account_id'])   
-        button_runbook= col8.empty()  # create a placeholder
-        button_remediate= col9.empty()  # create a placeholder
-        runbook_action = button_runbook.button("Runbook", key=f"{data['account_id']}-rb",args=(data['account_id'],data['alarmData']['description'],), on_click=get_runbook)
-        remediate_action = button_remediate.button("Remediate", key=f"{data['account_id']}-re", args=(data['account_id'],data['alarmData']['description']), on_click=alert_remediate)
+    if remediate_action:
+        if pk is None:
+            st.write("Please select an incident")
+            return
+        with st.spinner("Processing"):
+            incident = incident_remediate(pk,description)
+            st.write(incident['result'])
--- src/ui_deploy/utils/apigw_handler.py
+++ src/ui_deploy/utils/apigw_handler.py
@@ -12,7 +12,6 @@ AWS_REGION=os.getenv('AWS_REGION')
 def get_incidents(incidentStatus):
     headers = {'Authorization': f"{st.session_state['token']}", 'Content-Type': 'application/json'}
     url = f'{APIGWURL}{APIGWSTAGE}/get-incidents'
-    #url = f'{APIGWURL}{APIGWSTAGE}/active-alerts'
     try:
        response = requests.get(url,params={"incidentStatus": incidentStatus}, headers = headers)
        response.raise_for_status()
@@ -24,31 +23,25 @@ def get_incidents(incidentStatus):
 
 
 def get_runbook(account_id,description):
-    st.write(account_id)
-    print("Calling the get_runbook")
     headers = {'Authorization': f"{st.session_state['token']}", 'Content-Type': 'application/json'}
     url = f'{APIGWURL}{APIGWSTAGE}/get-incident-runbook'
     try:
        response = requests.get(url, params={"query": description}, headers = headers)
        response.raise_for_status()
-       print(response.json())
-       st.write(response.json())
        return response.json()
     except requests.exceptions.RequestException as e:
        print (f"Error in calling /alerts API: {e}")
        return None
 
-def alert_remediate(account_id,description):
-    st.write(account_id)
+def incident_remediate(account_id,description):
     print("Calling the agent to take action")
     headers = {'Authorization': f"{st.session_state['token']}", 'Content-Type': 'application/json'}
     url = f'{APIGWURL}{APIGWSTAGE}/post-incident-action'
-    print (url)
     try:
        data = {'action':description}
        response = requests.post(url, headers = headers, json=data)
        response.raise_for_status()
-       st.write(response.json())
+       return response.json()
     except requests.exceptions.RequestException as e:
        print (f"Error in calling /alerts API: {e}")
        return None   
